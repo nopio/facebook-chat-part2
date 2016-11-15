c0130b2c4810fa8314256a60dedd7698901f997c
Piotr Jaworski <piotr.jaworski@live.com>
2016-11-15 22:53:37 +0100

    Start a converastion

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,6 +5,19 @@ class Conversation < ApplicationRecord
 
   validates :sender_id, uniqueness: { scope: :recipient_id }
 
+  scope :between, -> (sender_id, recipient_id) do
+    where(sender_id: sender_id, recipient_id: recipient_id).or(
+      where(sender_id: recipient_id, recipient_id: sender_id)
+    )
+  end
+
+  def self.get(sender_id, recipient_id)
+    conversation = between(sender_id, recipient_id).first
+    return conversation if conversation.present?
+
+    create(sender_id: sender_id, recipient_id: recipient_id)
+  end
+
   def opposed_user(user)
     user == recipient ? sender : recipient
   end
